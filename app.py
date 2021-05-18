427ac126540fd3ceafa4747658b4556c1128d371
favuur <fankyfunky29@gmail.com>
2021-05-18 01:33:03 +0100

    started using beautifulSoup

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, request, render_template
+import re
+from bs4 import BeautifulSoup
+import urllib.request
+
 app = Flask(__name__)
+app.run(use_reloader=True)
+
+def performScrape(target_url):
+    with urllib.request.urlopen(target_url) as response:
+        html=response.read()
+        soup=BeautifulSoup(html,"html.parser").b
+        
+        print(soup)
 
 @app.route('/')
 def hello_world():
@@ -8,4 +20,13 @@ def hello_world():
 @app.route('/scrape')
 def handle_scrape():
     target_url=request.args.get('target_url')
-    return 'Handling scrape data for' + target_url
+
+    expression = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    isAUrl = re.match(expression, target_url)
+
+    if (isAUrl) :
+        performScrape(target_url)
+
+    return "TARGET URL provided is not a URL Format E.g https://google.com"
+    
+    
