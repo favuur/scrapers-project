09dc8cfed2fd3cc25a31d30c66058b2e9f0528f7
victory <vickywane@gmail.com>
2021-05-19 21:56:01 +0100

    update: improved web styling

--- app.py
+++ app.py
@@ -3,43 +3,48 @@ import re
 from bs4 import BeautifulSoup
 import urllib.request
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='./static',
+            template_folder='./templates'
+            )
 app.run(use_reloader=True)
+
 
 def performScrape(target_url):
     with urllib.request.urlopen(target_url) as response:
-        html=response.read()
-        soup=BeautifulSoup(html,"html.parser")
+        html = response.read()
+        soup = BeautifulSoup(html, "html.parser")
 
         # LIST OF TAGS
         # LOOP THROUGH THE LIST A, P, 
 
-        tags = [ "a", "b", "p", "div", "i", "abbr", "address", "base", "button", "input", "table", "sub", "table", "title", "u", "var"  ]
-        
-        totalElement = 0;
-        
+        tags = ["a", "b", "p", "div", "i", "abbr", "address", "base", "button", "input", "table", "sub", "table",
+                "title", "u", "var"]
+
+        totalElement = 0
+
         for element in tags:
-            elementList=soup.find_all(element)
+            elementList = soup.find_all(element)
             totalElement = len(elementList) + totalElement
             for tag in elementList:
                 print(tag.text)
 
         print("Ran a foor loop over: {} tags".format(totalElement))
-    
+
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/scrape')
 def handle_scrape():
-    target_url=request.args.get('target_url')
+    target_url = request.args.get('target_url')
 
     expression = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     isAUrl = re.match(expression, target_url)
 
-    if (isAUrl) :
+    if isAUrl:
         performScrape(target_url)
 
     return "TARGET URL provided is not a URL Format E.g https://google.com"
-    
-    
